e95572c33ab86587c60694ebf22ca5dd19cb2f9e
leon <tmkook@gmail.com>
2025-12-25 01:10:15 +0800

    fix bug

--- main.js
+++ main.js
@@ -6,10 +6,8 @@ import { vchat } from 'vchat'
  * 避免操作太快导致异常
  */
 vchat.onMessage((notice) => {
-    vchat.openApp() //打开APP
-    vchat.openTopSession() //打开第一个会话
-    vchat.sendText("Hello world") //发送文字消息
-    vchat.finish() //结束任务
-    // notice.getTitle() //通知标题
-    // notice.getText() //通知详情
+    vchat.openApp()
+    vchat.openTopSession()
+    vchat.sendText("Hello world")
+    vchat.finish()
 })
